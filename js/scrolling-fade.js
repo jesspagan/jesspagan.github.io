ad9f4b7627a9aff723849af29c44c98d31576c17
Jess Pagan <jessica.pagan3@upr.edu>
2015-09-24 23:29:15 -0400

    Template completed

---
+++ js/scrolling-fade.js
@@ -0,0 +1,15 @@
+$(window).scroll(function(){
+
+	/* Check the location of each desired element */
+        $('.fader').each( function(i){
+            
+            a = $(this).offset().top + $(this).height();
+        	b = $(window).scrollTop() + $(window).height()/3;
+        	if (a < b) {
+        		if ($(this).css('opacity')==1) $(this).fadeTo(1000,0);
+        	}
+        	else {
+        		if ($(this).css('opacity')==0) $(this).fadeTo(1000,1);
+        	}
+        }); 
+  });
